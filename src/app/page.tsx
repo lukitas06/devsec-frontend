0735feaa8c7762b5d26aa1292a1288ea59ac1831
Lucas Alegre <lucasalegre@192.168.1.6>
2025-05-06 10:44:35 -0300

    initial commit

---
+++ src/app/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import LogDashboard from 'components/logs/logDashboard';
+
+export default function Home() {
+
+  return (
+    <div>
+      <h1>Log Dashboard</h1>
+      <LogDashboard />
+    </div>
+  );
+}
